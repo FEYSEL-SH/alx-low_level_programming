4fc6300d36d2a4e720bf8380f1f7a1d0b773612a
feya <feya@gmail.com>
2023-03-27 06:50:58 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,11 +4,10 @@
 /**
  * File: main.h
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
+ *       used in the 0x05-pointers_arrays_strings directo
+ *       /
  */
-
 #include <stdlib.h>
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
