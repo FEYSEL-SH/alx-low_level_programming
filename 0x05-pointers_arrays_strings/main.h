5b50c8b92236496bbb65f31c69bcd1d5e682e54d
feya <feya@gmail.com>
2023-03-27 05:48:19 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-#include <unisted.h>
+
+/**
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
+
+#include <stdlib.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -14,5 +20,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
-#endif 
+#endif
